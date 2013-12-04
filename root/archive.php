8709269deb5d0bfdfd12a69439bbbe5dcbf4bd4e
Kazue Igarashi <gatespace.k@gmail.com>
2013-12-04 14:35:23 +0900

    _s: Add closing brackets to avoid fatal error.

--- root/archive.php
+++ root/archive.php
@@ -39,10 +39,10 @@ get_header(); ?>
 							printf( __( 'Day: %s', '{%= prefix %}' ), '<span>' . get_the_date() . '</span>' );
 
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', '{%= prefix %}' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '{%= prefix %}' ) . '</span>' ) );
+							printf( __( 'Month: %s', '{%= prefix %}' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '{%= prefix %}' ) ) . '</span>' );
 
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', '{%= prefix %}' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '{%= prefix %}' ) . '</span>' ) );
+							printf( __( 'Year: %s', '{%= prefix %}' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '{%= prefix %}' ) ) . '</span>' );
 
 						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
 							_e( 'Asides', '{%= prefix %}' );
