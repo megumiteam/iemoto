c04f3251d02052f06ad6784f81f11c33dacd40f2
Kazue Igarashi <igarashi@digitalcube.jp>
2014-06-02 15:29:55 +0900

    _s commit [91d2514]
    * _s: List post formats in their own line.

--- root/functions.php
+++ root/functions.php
@@ -57,7 +57,9 @@ function {%= prefix %}_setup() {
 	 * Enable support for Post Formats.
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link'
+	) );
 
 	// Setup the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( '{%= prefix %}_custom_background_args', array(
