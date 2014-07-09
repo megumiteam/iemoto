706fd032ea16f9663b56ccf5c2642662b30bd015
Kazue Igarashi <gatespace.k@gmail.com>
2014-07-09 11:49:30 +0900

    [bugfix] search.php
    sidebar.php と search.php 間違えてしまってました

--- root/search.php
+++ root/search.php
@@ -5,11 +5,45 @@
  * @package {%= title %}
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
-?>
+get_header(); ?>
 
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
+	<section id="primary" class="content-area">
+		<?php do_action( '{%= prefix %}_before_primary' ); ?>
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php do_action( '{%= prefix %}_before_page_header' ); ?>
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', '{%= prefix %}' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<?php do_action( '{%= prefix %}_after_page_header' ); ?>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+				/**
+				 * Run the loop for the search to output the results.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-search.php and that will be used instead.
+				 */
+				get_template_part( 'content', 'search' );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php {%= prefix %}_paging_nav(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+		<?php do_action( '{%= prefix %}_after_primary' ); ?>
+	</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
