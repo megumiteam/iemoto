0ecb719ea314dee2782177867b88e432cc4251a2
Kazue Igarashi <igarashi@digitalcube.jp>
2014-11-13 11:46:08 +0900

    update template files.
    * Improve menu toggle accessibility.
    see https://github.com/Automattic/_s/commit/e142af3a2720e38179e294181562c22842a68c3e
    
    * Use `div`s consistently throughout templates.
    see https://github.com/Automattic/_s/commit/6c783cd09f68137f1ee0ec6dda1e7d9b0d79c8ec
    
    * _s: Introduce 4.1 archive template tags.
    see https://github.com/Automattic/_s/commit/bb867c0fcb2cfbae298d44a87c0638c75b8739f8
    
    * update language files.
    see https://github.com/gatespace/_s/commit/1228ca58721b64438712d9461369583d95ee30bb

--- root/archive.php
+++ root/archive.php
@@ -4,76 +4,20 @@
  *
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
- * @package {%= title %}
+ * @package _s
  */
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', '{%= prefix %}' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', '{%= prefix %}' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', '{%= prefix %}' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '{%= prefix %}' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', '{%= prefix %}' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '{%= prefix %}' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', '{%= prefix %}' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', '{%= prefix %}' );
-
-						else :
-							_e( 'Archives', '{%= prefix %}' );
-
-						endif;
-					?>
-				</h1>
 				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
@@ -99,7 +43,7 @@ get_header(); ?>
 		<?php endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
