7e00a71f69c13a1ba5b32045a68febf8824eb7a4
Kazue Igarashi <igarashi@digitalcube.jp>
2015-01-08 11:04:05 +0900

    [UPDATE] _s commit [e5e31d4] to [6b2e7c8]
    * Update WP versions that are being used in Travis checks. [skip ci]
    * Update Travis CI build checks. Props @miya0001.
    * Use core navigation template tags.
    ** Remove custom arguments from `the_posts_navigation()` and `the_post_navigation()`.
    **  Adjust fallbacks for `the_posts_navigation()` and `the_post_navigation()`.
    **  update pot file.
    **  merge master for style.css and style.scss.
    * update ja.mo.

--- root/single.php
+++ root/single.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
-			<?php {%= prefix %}_post_nav(); ?>
+			<?php the_post_navigation(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
