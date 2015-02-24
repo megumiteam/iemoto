4248b9b9538b3a479f6b68d40b1d5a8eb1b2c230
Kazue Igarashi <igarashi@digitalcube.jp>
2015-02-24 11:11:22 +0900

    [UPDATE] _s commit.
    * Consistent navigation.
    * Sanitize location.hash before passing it to getElementById.
    * Fix JSLint error
    * Using best practices to apply the proper units to the font-size mixin.

--- root/comments.php
+++ root/comments.php
@@ -32,10 +32,14 @@ if ( post_password_required() ) {
 		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', '{%= prefix %}' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', '{%= prefix %}' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', '{%= prefix %}' ) ); ?></div>
+		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', '{%= prefix %}' ); ?></h2>
+			<div class="nav-links">
+
+				<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', '{%= prefix %}' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', '{%= prefix %}' ) ); ?></div>
+
+			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
 		<?php endif; // check for comment navigation ?>
 
@@ -49,10 +53,14 @@ if ( post_password_required() ) {
 		</ol><!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', '{%= prefix %}' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', '{%= prefix %}' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', '{%= prefix %}' ) ); ?></div>
+		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', '{%= prefix %}' ); ?></h2>
+			<div class="nav-links">
+
+				<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', '{%= prefix %}' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', '{%= prefix %}' ) ); ?></div>
+
+			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
 		<?php endif; // check for comment navigation ?>
 
