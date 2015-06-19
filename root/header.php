a0b4d7e115e77d30f1e5e9d2ee3cce03187db615
Kazue Igarashi <igarashi@digitalcube.jp>
2015-06-19 10:21:48 +0900

    [UPDATE] _s [4e84c58]
    Merge pull request #763 from sagarjadhav/master
    
    Updated Tagline markup as per W3C guidelines.
    
    see: https://github.com/Automattic/_s/commit/4e84c580a0616e2745920c9b7e55a3b5344d4086

--- root/header.php
+++ root/header.php
@@ -29,7 +29,7 @@
 
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
