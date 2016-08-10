6ffc304d34d2e38c71f4ee9c979b80130051f58c
Kazue Igarashi <igarashi@digitalcube.jp>
2016-08-10 12:01:52 +0900

    [merge] _s commit.
    - Fix skip to content link.
    - Clean up trailing whitespace
    - Clean up trailing whitespace
    - Using tabs instead of mixed tabs/spaces
    - Tested up to: 4.5.3 version bump.
    - change .taxonomy-description to more accurate .archive-description
    - Use consistent syntax for control structures

--- root/template-parts/content.php
+++ root/template-parts/content.php
@@ -13,11 +13,11 @@
 	<header class="entry-header">
 		<?php do_action( '{%= prefix %}_before_entry_header' ); ?>
 		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
